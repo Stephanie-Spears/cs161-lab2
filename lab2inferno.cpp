25df3d86b45169b53a5e77cb418426d74a600340
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-08 11:34:03 -0800

    upload

---
+++ lab2inferno.cpp
@@ -0,0 +1,6 @@
+
+#include <iostream>
+int main ()
+{
+	std::cout << "Midway upon the journey of our life \nI found myself within a forest dark, \nFor the straightforward pathway had be lost. \n-Dante Alighieri \n" ;
+}
